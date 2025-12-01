9dd65491956e47947e68758a7dc9cc93672f59ef
paula-cediel <mariapaulacediel08@gmail.com>
2025-12-01 16:34:59 -0500

    Implement product selection and cloning feature
    Added functionality to select and clone products.

--- home/Script.js
+++ home/Script.js
@@ -57,3 +57,26 @@ document.addEventListener("DOMContentLoaded", () => {
 
     contenedor.innerHTML = html;
 });
+
+    const seleccionados = document.getElementById('seleccionados');
+
+    // Espera a que los productos de tu compañero existan
+
+    document.addEventListener('click', (e) => {
+        if (e.target.closest('.productos')) {
+            const prod = e.target.closest('.productos');
+
+            // Marcar el producto original
+            prod.classList.add('seleccionado');
+
+            // Clonar producto seleccionado
+            const copia = prod.cloneNode(true);
+            copia.classList.remove('seleccionado');
+
+            // Enviar a la sección de seleccionados
+            seleccionados.appendChild(copia);
+        }
+    });
+
+
+
