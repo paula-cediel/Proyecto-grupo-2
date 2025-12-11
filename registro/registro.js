99cab22a94647920f9a22a89f7618d7479760bc7
Ricardo <rsandovalort@gmail.com>
2025-12-11 14:55:16 -0500

    Vista registro

--- registro/registro.js
+++ registro/registro.js
@@ -1,27 +1,30 @@
-//Variables para elementos del formulario
 const formulario = document.getElementById("formulario");
-const nombre = document.getElementById("nombre").value.trim();
-const telefono = document.getElementById("telefono").value.trim();
-const email = document.getElementById("email").value.trim();
-const contrasena = document.getElementById("contrasena").value.trim();
 
 //Variables para recorrer y hacer validaciones
 const minusculas = "abcdefghijklmnopqrstuvwxyz".split("");
 const mayusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 const numeros = "0123456789".split("");
-const simbolo = "@$!%*?&".split("");
+const simbolos = "@$!%*?&".split("");
 
-//Variables para validacion de correo
-let min = false;
-let may = false;
-let num = false;
-let sim = false;
+
 
 //Variables para verificacion de contenidos
 
 //Funcion-boton para capturar la info del fomulario
-form.addEventListener("submit", function (event) {
+formulario.addEventListener("submit", function (event) {
     event.preventDefault();
+
+    //Variables para elementos del formulario
+    const nombre = document.getElementById("nombre").value.trim();
+    const telefono = document.getElementById("telefono").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const contrasena = document.getElementById("contrasena").value.trim();
+
+    //Variables para validacion de correo
+    let min = false;
+    let may = false;
+    let num = false;
+    let sim = false;
 
     //Validaciones con SweetAlert
     if (nombre === "" || telefono === "" || email === "" || contrasena === "" ) {
@@ -31,20 +34,17 @@ form.addEventListener("submit", function (event) {
     if (!isNaN(nombre)) {
     return Swal.fire("Error", "El nombre no puede ser solo números.", "error");
     }
-    if (nombre.length < 3 || nombre.length > 20 || nombre.includes(" ")) {
-            return Swal.fire("Error", "El nombre de usuario debe tener entre 3 y 20 caracteres y no debe contener espacios.", "error");
-    }
     if (telefono.length !== 10 || telefono.includes(" ")) {
     return Swal.fire("Error", "El teléfono debe tener 10 números y no debe contener espacios.", "error");
     }
-    if (telefono) {
+    if (isNaN(telefono)) {
     return Swal.fire("Error", "El teléfono solo debe contener números.", "error");
-    }
+}
     if (contrasena.length < 8 || contrasena.includes(" ")) {
         return Swal.fire("Error", "La contraseña debe tener mínimo 8 caracteres y no tener espacios", "error");
     }
     if (contrasena === nombre) {
-    return Swal.fire("Error", "La contraseña no puede ser igual al usuario o correo.", "error");
+    return Swal.fire("Error", "La contraseña no puede ser igual al usuario.", "error");
     }
     for (let letra of minusculas) {
         if (contrasena.includes(letra)) {
@@ -64,7 +64,7 @@ form.addEventListener("submit", function (event) {
             break;
         }
     }
-    for (let simbolo of especiales) {
+    for (let simbolo of simbolos) {
        if (contrasena.includes(simbolo)) {
             sim = true;
             break;
@@ -76,7 +76,7 @@ form.addEventListener("submit", function (event) {
     }
 
     //Creación del objeto usuario
-    const usuario = { nombre, telefono, email, constrasena };
+    const usuario = { nombre, telefono, email, contrasena };
 
     //Conversión del usuario en JSON
     localStorage.setItem("usuario", JSON.stringify(usuario));
@@ -85,10 +85,10 @@ form.addEventListener("submit", function (event) {
     Swal.fire("Registrado", "Usuario guardado correctamente");
 
     //Limpieza de campo del formulario
-    nombre.value = "";
-    telefono.value = "";
-    email.value = "";
-    contrasena.value = "";
+    document.getElementById("nombre").value = "";
+    document.getElementById("telefono").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("contrasena").value = "";
 });
 
 
