5040ee1cee6e7fae3ed55ecc3488827223bb57c2
Paula <mariapaulacediel08@gmail.com>
2026-01-13 16:20:14 -0500

    algo de reseñas

--- home/Script.js
+++ home/Script.js
@@ -77,6 +77,18 @@ document.addEventListener("DOMContentLoaded", () => {
             seleccionados.appendChild(copia);
         }
     });
+card.querySelector(".like").onclick=()=>{
+  const likeBtn = card.querySelector(".like");
+  const count = card.querySelector(".like-count");
+
+  if (likeBtn.classList.contains("text-danger")) {
+    likeBtn.classList.remove("text-danger");
+    count.textContent = Number(count.textContent) - 1;
+  } else {
+    likeBtn.classList.add("text-danger");
+    count.textContent = Number(count.textContent) + 1;
+  }
+};
 
 
 
